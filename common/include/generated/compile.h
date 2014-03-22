7b95c6a8bcac19b2f02564f8d0347f824edd24c8
angpysha <angpysha@gmail.com>
2014-03-22 14:59:36 +0200

    first commit

---
+++ common/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 6 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#6 PREEMPT Fri Mar 21 16:58:47 EET 2014"
+#define LINUX_COMPILE_TIME "16:58:47"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "andrey-System-Product-Name"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-67) "
